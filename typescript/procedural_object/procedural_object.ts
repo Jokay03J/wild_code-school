18d0ab38073c4f7f8a8db2583f0ea50e9acdd2fe
joan54 <joan@kolus.io>
2024-09-23 16:48:28 +0200

    typescript-procedural_objet

---
+++ typescript/procedural_object/procedural_object.ts
@@ -0,0 +1,24 @@
+class Person {
+    private name: string;
+    private age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    tellMyName(): void {
+        console.log(`I am ${this.name}`);
+    }
+
+    tellMyAge(): void {
+        console.log(`I am ${this.age} years old`);
+    }
+}
+
+const john = new Person('John', 40);
+const mary = new Person('Mary', 35);
+john.tellMyName();
+john.tellMyAge();
+
+mary.tellMyName();
+mary.tellMyAge();
